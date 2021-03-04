bb7fc09a642d1893ae256b723f758494187823ae
Danielle Orley <danielle.m.orley1993@gmail.com>
2021-03-04 08:21:49 -0800

    working on index.js

--- index.js
+++ index.js
@@ -1,6 +1,42 @@
-//To Do: packages
+//packages required
 const inquirer = require('inquirer');
-const fs = require("fs");
+const fs = require('fs');
+
 
 //To Do: other links
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
+const { ENOTEMPTY } = require('constants');
 
+const teamArr = [];
+
+
+function promptManager() {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "Enter Manager's name:",
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "Enter Manager's id:"
+        },
+        {
+            type: "input",
+            name: "office",
+            message: "Enter the Manager's office number"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Enter the Manager's email"
+        }
+    ]).then(function({name, id, office, email}) {
+        let manager = new Manager(name, id, office, email);
+        teamArr.push(manager);
+        
+    })
+}
